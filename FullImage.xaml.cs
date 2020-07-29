fc3ae05f225d6dfaa1764c00d254591b62711811
Mihaela892 <64497086+Mihaela892@users.noreply.github.com>
2020-07-30 00:24:32 +0300

    Add files via upload

---
+++ FullImage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MultiMediaPickerSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MultiMediaPickerSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FullImage : ContentPage
+    {
+       
+
+        public FullImage()
+        {
+            InitializeComponent();
+
+          
+
+        }
+    }
+}
---
+++ MediaTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using MultiMediaPickerSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MultiMediaPickerSample
+{
+    public class MediaTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate OneMediaFileMessageTemplate { get; set; }
+        public DataTemplate FourMediaFileMessageTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is MediaFile) return FourMediaFileMessageTemplate;
+            else return FourMediaFileMessageTemplate;
+
+        }
+    }
+}
